05e7321b043ce60f88bf491627339138694379f3
khalil kraiem <khalilmedkraiem@gmail>
2024-07-12 14:46:34 +0100

    microservice architecture WIP

--- src/microservices/multer/dto/update-multer.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMulterDto } from './create-multer.dto';
-
-export class UpdateMulterDto extends PartialType(CreateMulterDto) {
-  id: number;
-}
