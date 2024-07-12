15ffbf3b9421a7ecee6a29a1ef4552e9a27aec07
khalil kraiem <khalilmedkraiem@gmail>
2024-07-12 11:50:12 +0100

    nestjs/microservice package and config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('JALYSS API')
+      .setDescription('The JALYSS API description')
+      .setVersion('1.0')
+      .addTag('JALYSS')
+      .addApiKey(
+        { type: 'apiKey', name: 'Authorization', in: 'header' },
+        'apiKey',
+      )
+      .build();
+      const document = SwaggerModule.createDocument(app, config);
+      SwaggerModule.setup('api-docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
