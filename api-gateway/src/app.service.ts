f1f79407b17cc4d12b192dbde66d6c968025427b
khalil kraiem <khalilmedkraiem@gmail>
2024-07-18 18:06:23 +0100

    configEnv example in apigateway app

--- api-gateway/src/app.service.ts
+++ api-gateway/src/app.service.ts
@@ -1,19 +1,22 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateUserDto } from './create-user-dto';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('USER_MICROSERVICE') private readonly userClient: ClientProxy,
+    private readonly prisma: PrismaService,
   ) {}
   getHello(): string {
     return 'hello Jalyss API-GATEWAY';
   }
-  regiterUser(data:CreateUserDto) {
+  regiterUser(data: CreateUserDto) {
     return this.userClient.send({ cmd: 'register_user' }, data);
   }
-  getUser() {
-    return this.userClient.send({ cmd: 'get_user' }, {});
+  async getUser() {
+    // return this.userClient.send({ cmd: 'get_user' }, {});
+    return await this.prisma.user.findMany();
   }
 }
