83cbb4679ec5aa6403966a75052b8a85569d7863
siwar <siwarzelfanitn@gmail.com>
2024-07-18 18:53:23 +0200

    stocks-module

--- microservices/users-module/src/app.module.ts
+++ microservices/users-module/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'nestjs-prisma';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule.forRoot({isGlobal:true})],
+  imports: [PrismaModule.forRoot({ isGlobal: true }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
