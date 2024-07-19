1cfe51ba99b44244be99dbab20a6e5248361760b
SBoughrara <seif.eddin.boughrara7@gmail.com>
2024-07-19 18:25:23 +0100

    AUTH MICROSERVICE

---
+++ microservices/auth-microservice/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from 'nestjs-prisma';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    PrismaModule.forRoot({ isGlobal: true }),AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
