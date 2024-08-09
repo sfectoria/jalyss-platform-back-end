16c9dc197e656bcb621e02cc02beefc653586a05
khalil kraiem <khalilmedkraiem@gmail>
2024-08-09 12:07:35 +0100

    selling Invoce with exitNote creating

--- microservices/selling-module/src/sales-invoices/dto/create-sales-invoice.dto.ts
+++ microservices/selling-module/src/sales-invoices/dto/create-sales-invoice.dto.ts
@@ -1,20 +1,21 @@
-import { ApiProperty } from "@nestjs/swagger";
-
+import { ApiProperty } from '@nestjs/swagger';
 
 class venteFacture_line {
-    @ApiProperty()
-    articleId: number;
+  @ApiProperty()
+  articleId: number;
 }
 
 export class CreateSalesInvoiceDto {
-    @ApiProperty()
-    id_bon_commande: number
-    @ApiProperty()
-    bonSortieId: number
-    @ApiProperty()
-    stockId: number
-    @ApiProperty()
-    date : string
-    @ApiProperty({type: [venteFacture_line]})
-    venteFacture_lines: venteFacture_line[]
+  @ApiProperty()
+  id_bon_commande?: number;
+  @ApiProperty()
+  bonSortieId: number;
+  @ApiProperty()
+  id_client: number;
+  @ApiProperty()
+  saleChannelId: number;
+  @ApiProperty()
+  date: string;
+  @ApiProperty({ type: [venteFacture_line] })
+  venteFacture_lines: venteFacture_line[];
 }
