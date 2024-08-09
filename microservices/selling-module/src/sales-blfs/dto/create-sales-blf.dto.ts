a3c864891c82ae1ce79f519a1de93d4f2600f701
siwar <siwarzelfanitn@gmail.com>
2024-08-09 15:53:05 +0200

    create invoice sales with and without bonSotrieId

--- microservices/selling-module/src/sales-blfs/dto/create-sales-blf.dto.ts
+++ microservices/selling-module/src/sales-blfs/dto/create-sales-blf.dto.ts
@@ -1,6 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+class VenteBLFacture {
+  @ApiProperty()
+  articleId: number;
+}
+
 export class CreateSalesBlfDto {
-  sales_channelsId: number;
+  @ApiProperty()
+  bonCommandeId?: number;
+  @ApiProperty()
   bonSortieId: number;
+  @ApiProperty()
   clientId: number;
+  @ApiProperty()
+  sales_channelsId: number;
+  @ApiProperty()
   delivery_date: string;
+  @ApiProperty({ type: [VenteBLFacture] })
+  venteBLFacture_lines: VenteBLFacture[];
 }
--- microservices/selling-module/src/sales-bls/sales-bls.service.ts
+++ microservices/selling-module/src/sales-bls/sales-bls.service.ts
@@ -7,8 +7,53 @@ import { PrismaService } from 'nestjs-prisma';
 export class SalesBlsService {
   constructor(private readonly prisma : PrismaService) {}
   async create(createSalesBlDto: CreateSalesBlDto) {
-    return await this.prisma.venteBonLivraison.create({data:createSalesBlDto})
-  }
+    return await this.prisma.$transaction(async (prisma) => {
+      const { bonSortieId, id_bon_commande, venteBonLivraison_lines, ...rest } =
+      createSalesBlDto;
+      if (!bonSortieId) {
+        // kif nji nasna3 bon de sorti lazemni naaref stockId 3lech
+        // 3la khater kif nasnaa bon sorti lazem aandha num te3ha
+        //eli houwa last +1 fi stock heka mouch fil kol
+        const lastExitNoteOfStock = (
+          await prisma.bonSortie.findMany({
+            where: {
+              Stock: {
+                sales_channels: {
+                  some: { id: createSalesBlDto.saleChannelId },
+                },
+              },
+            },
+            take: 1,
+            orderBy: {
+              num_bonSortie: 'desc',
+            },
+          })
+        )[0];
+        const newExitNote = await prisma.bonSortie.create({
+          data: {
+            num_bonSortie: lastExitNoteOfStock.num_bonSortie,
+            stockId: 5,
+            sortie_date: new Date(createSalesBlDto.delivery_date).toISOString(),
+            BonSortie_line: {
+              createMany: {
+                data: createSalesBlDto.venteBonLivraison_lines,
+              },
+            },
+          },
+        });
+
+        await prisma.venteFacture.create({
+          data: {
+            ...rest,
+            date: new Date(rest.delivery_date).toISOString(),
+            venteFacture_line: {
+              createMany: { data: createSalesBlDto.venteBonLivraison_lines },
+            },
+            bonSortieId: newExitNote.id,
+          },
+        });
+      }
+    });  }
 
   async findAll() {
     return await this.prisma.venteBonLivraison.findMany();
