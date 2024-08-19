9280741e5878b8f8e0d46b29d17f7b2b11f1733f
siwar <siwarzelfanitn@gmail.com>
2024-08-19 16:48:56 +0200

    sales delivery note and invoice and delivery note invoice done

--- microservices/selling-module/src/sales-delivery-invoice/sales-delivery-invoice.service.ts
+++ microservices/selling-module/src/sales-delivery-invoice/sales-delivery-invoice.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { CreateSalesDeliveryInvoiceDto } from './dto/create-sales-delivery-invoice.dto';
 import { UpdateSalesDeliveryInvoiceDto } from './dto/update-sales-delivery-invoice.dto';
 import { PrismaService } from 'nestjs-prisma';
+import { ExitNote } from 'src/helpers/exitNote';
+import {  Prisma } from '@prisma/client';
+
 
 @Injectable() //vente bon de livraison facture
 export class SalesDeliveryInvoiceService {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(
+    private readonly prisma: PrismaService,
+    private helperExitNote: ExitNote,
+  ) { }
   async create(createSalesDeliveryInvoiceDto: CreateSalesDeliveryInvoiceDto) {
-    return await this.prisma.$transaction(async (prisma) => {
+    return await this.prisma.$transaction(async (prisma : Prisma.TransactionClient) => {
       let { exitNoteId, purchaseOrderId, salesDeliveryInvoicelines, ...rest } =
         createSalesDeliveryInvoiceDto;
 
@@ -15,45 +21,28 @@ export class SalesDeliveryInvoiceService {
         // kif nji nasna3 bon de sorti lazemni naaref stockId 3lech
         // 3la khater kif nasnaa bon sorti lazem aandha num te3ha
         //eli houwa last +1 fi stock heka mouch fil kol
-        const lastExitNoteOfStock = (
-          await prisma.exitNote.findMany({
-            where: {
-              stock: {
-                salesChannels: {
-                  some: { id: createSalesDeliveryInvoiceDto.salesChannelsId },
-                },
-              },
-            },
-            take: 1,
-            orderBy: {
-              numExitNote: 'desc',
-            },
-          })
-        )[0];
-        const newExitNote = await prisma.exitNote.create({
+
+        const newExitNote = await this.helperExitNote.create(
+          prisma,
+          // 'invoice',
+          {
+            saleChannelId: createSalesDeliveryInvoiceDto.salesChannelsId,
+            date: createSalesDeliveryInvoiceDto.deliveryDate,
+            exitNoteLines: salesDeliveryInvoicelines,
+          },
+        );
+        exitNoteId = newExitNote.id;
+        return await prisma.salesDeliveryInvoice.create({
           data: {
-            numExitNote: lastExitNoteOfStock.numExitNote,
-            stockId: 5,
-            exitDate: new Date(createSalesDeliveryInvoiceDto.deliveryDate).toISOString(),
-            exitNoteLine: {
-              createMany: {
-                data: createSalesDeliveryInvoiceDto.salesDeliveryInvoicelines,
-              },
+            ...rest,
+            deliveryDate: new Date(rest.deliveryDate).toISOString(),
+            salesDeliveryInvoiceLine: {
+              createMany: { data: salesDeliveryInvoicelines },
             },
+            exitNoteId,
           },
         });
-        exitNoteId = newExitNote.id;
       }
-      return await prisma.salesDeliveryInvoice.create({
-        data: {
-          ...rest,
-          deliveryDate: new Date(rest.deliveryDate).toISOString(),
-          salesDeliveryInvoiceLine: {
-            createMany: { data: createSalesDeliveryInvoiceDto.salesDeliveryInvoicelines },
-          },
-          exitNoteId,
-        },
-      });
     });
   }
 
