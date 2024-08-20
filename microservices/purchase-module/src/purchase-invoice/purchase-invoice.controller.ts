320803d500f1385cc47cbb541ccdac50fc33b6a9
rawen10 <rawensoltqni@gmail.com>
2024-08-20 14:04:59 +0200

    pulling from main

--- microservices/purchase-module/src/purchase-invoice/purchase-invoice.controller.ts
+++ microservices/purchase-module/src/purchase-invoice/purchase-invoice.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PurchaseInvoiceService } from './purchase-invoice.service';
 import { CreatePurchaseInvoiceDto } from './dto/create-purchase-invoice.dto';
 import { UpdatePurchaseInvoiceDto } from './dto/update-purchase-invoice.dto';
+import { Payload } from '@nestjs/microservices';
 
 @Controller('purchase-invoice')
 export class PurchaseInvoiceController {
   constructor(private readonly purchaseInvoiceService: PurchaseInvoiceService) {}
 
   @Post()
-  create(@Body() createPurchaseInvoiceDto: CreatePurchaseInvoiceDto) {
+  create(@Payload() createPurchaseInvoiceDto: CreatePurchaseInvoiceDto) {
     return this.purchaseInvoiceService.create(createPurchaseInvoiceDto);
   }
 
@@ -18,17 +19,17 @@ export class PurchaseInvoiceController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Payload('id') id: string) {
     return this.purchaseInvoiceService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePurchaseInvoiceDto: UpdatePurchaseInvoiceDto) {
+  update(@Payload('id') id: string, @Body() updatePurchaseInvoiceDto: UpdatePurchaseInvoiceDto) {
     return this.purchaseInvoiceService.update(+id, updatePurchaseInvoiceDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Payload('id') id: string) {
     return this.purchaseInvoiceService.remove(+id);
   }
 }
