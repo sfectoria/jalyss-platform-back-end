a035894b753a6f8ced37bd54109a7fba5d1f8360
Oussema Cherif <144437939+oussemacherif@users.noreply.github.com>
2024-08-24 10:40:18 +0100

    Delete microservices/client-microservice directory

--- microservices/client-microservice/dist/categorie_client/dto/create-categorie_client.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class CreateCategorieClientDto {
-    name: string;
-}
--- microservices/client-microservice/dist/categorie_client/dto/update-categorie_client.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateCategorieClientDto } from './create-categorie_client.dto';
-declare const UpdateCategorieClientDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateCategorieClientDto>>;
-export declare class UpdateCategorieClientDto extends UpdateCategorieClientDto_base {
-}
-export {};
