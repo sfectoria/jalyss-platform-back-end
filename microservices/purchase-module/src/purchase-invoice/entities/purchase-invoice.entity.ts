b382f8f299272865bda2516fa6177a2c2302da8a
siwar <siwarzelfanitn@gmail.com>
2024-09-02 20:08:00 +0200

    conflicts

--- microservices/purchase-module/src/purchase-invoice/entities/purchase-invoice.entity.ts
+++ microservices/purchase-module/src/purchase-invoice/entities/purchase-invoice.entity.ts
@@ -1,20 +1,7 @@
 export class PurchaseInvoice {}
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
 
->>>>>>> 0eae873a4c9c391374a59790e5d5dccca4810a74
-=======
->>>>>>> parent of 3ec5de1 (Merge branch 'main' into rawen)
 export class Filters {
     take: number;
     skip: number;
     receipNotesIds: number[];
-<<<<<<< HEAD
   }
->>>>>>> parent of 3ec5de1 (Merge branch 'main' into rawen)
-=======
-  }
->>>>>>> 0eae873a4c9c391374a59790e5d5dccca4810a74
--- microservices/selling-module/src/purchase-order/dto/create-purchase-order.dto.ts
+++ microservices/selling-module/src/purchase-order/dto/create-purchase-order.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, ApiTags } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
 import { TypePurchaseOrder } from '@prisma/client';
 
 class PurchaseOrderLine {
@@ -14,17 +14,8 @@ export class CreatePurchaseOrderDto {
   @ApiProperty()
   salesChannelsId: number;
   @ApiProperty()
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-  status : TypePurchaseOrder;
-=======
-  exitNoteId: number;
->>>>>>> parent of 3ec5de1 (Merge branch 'main' into rawen)
-  @ApiProperty()
   status : TypePurchaseOrder;
   @ApiProperty()
->>>>>>> 0eae873a4c9c391374a59790e5d5dccca4810a74
   orderDate: string;
   @ApiProperty()
   date: string;
