05555c1b99bdd68d82bc1084e5df45720a449b53
Oussema Cherif <ousseemacherif@gmail.com>
2024-09-16 10:37:19 +0100

    movement api gateway and microservice (in stock microservice) done

---
+++ api-gateway/src/modules/stocks/movements.service.ts
@@ -0,0 +1,41 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateReceiptNoteDto } from './dto/create-stock.dto';
+import { UpdateReceiptNoteDto } from './dto/update-stock.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { FiltersReceipt } from './entities/stock.entity';
+
+@Injectable()
+export class MovementsService {
+  constructor(@Inject ('STOCKS_MICROSERVICE') private readonly stocksClient: ClientProxy
+) {}
+ 
+  findAll(filters) {
+    console.log('find All receipt Note');
+    return this.stocksClient.send(
+      { cmd: 'all_movements' },
+      filters
+    )
+  }
+
+  findOne(id: number) {
+    console.log("hhh",id);
+    return this.stocksClient.send(
+      { cmd: 'one_movement' },
+      {id}
+    )
+  }
+
+//   update(id: number, updateReceiptNoteDto: UpdateReceiptNoteDto) {
+//     return this.stocksClient.send(
+//       { cmd: 'update_receiptNote' },
+//       {id, updateReceiptNoteDto}
+//     )
+//   }
+
+//   remove(id: number) {
+//     return this.stocksClient.send(
+//       { cmd: 'delete_receiptNote' },
+//       {id}
+//     )
+//   }
+}
--- microservices/stocks-module/src/modules/movements/movements.service.ts
+++ microservices/stocks-module/src/modules/movements/movements.service.ts
@@ -1,26 +1,78 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMovementDto } from './dto/create-movement.dto';
 import { UpdateMovementDto } from './dto/update-movement.dto';
+import { PrismaService } from 'nestjs-prisma';
 
 @Injectable()
 export class MovementsService {
-  create(createMovementDto: CreateMovementDto) {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createMovementDto: CreateMovementDto) {
     return 'This action adds a new movement';
   }
 
-  findAll() {
-    return `This action returns all movements`;
+  async findAll(filters) {
+    console.log('filters', filters);
+    let { take, skip } = filters;
+    take = !take ? 10 : +take;
+    skip = !skip ? 0 : +skip;
+    let where = {};
+    let exitNoteData = await this.prisma.exitNote.findMany({
+      include: {
+        exitNoteLine: { include: { Article: { include: { cover: true } } } },
+        stock: true,
+        transferNote: true,
+        salesDeliveryInvoice: { include: { client: true } },
+        salesDeliveryNote: { include: { client: true } },
+        salesInvoice: { include: { client: true } },
+      },
+    });
+    let receiptNoteData = await this.prisma.receiptNote.findMany({
+      include: {
+        receiptNoteLine: { include: { Article: { include: { cover: true } } } },
+        stock: true,
+        provider: true,
+        transferNote: true,
+        purchaseDeliveryInvoice: true,
+        purchaseDeliveryNote: true,
+        purchaseInvoice: true,
+      },
+    });
+
+    const mergeAndSortByDate = (exitNotes, receiptNotes) => {
+      const combined = [
+        ...exitNotes.map((item) => ({
+          ...item,
+          type: 'exit',
+          date: new Date(item.exitDate),
+          id: `exit-${item.id}`,
+        })),
+        ...receiptNotes.map((item) => ({
+          ...item,
+          type: 'receipt',
+          date: new Date(item.receiptDate),
+          id: `receipt-${item.id}`,
+        })),
+      ];
+
+      return combined.sort((a, b) => b.date - a.date);
+    };
+
+    const sortedData = mergeAndSortByDate(exitNoteData, receiptNoteData);
+    console.log(sortedData);
+    const piginatedData = sortedData.slice(skip, skip + take);
+    const count = sortedData.length;
+    return { data: piginatedData, count };
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} movement`;
   }
 
-  update(id: number, updateMovementDto: UpdateMovementDto) {
+  async update(id: number, updateMovementDto: UpdateMovementDto) {
     return `This action updates a #${id} movement`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} movement`;
   }
 }
