3791ca7cf00cc73b8bcd6c4c6c5bda6c1708f780
Oussema Cherif <ousseemacherif@gmail.com>
2024-09-28 09:55:54 +0100

    sales receipt microservice and aip gateway done

--- microservices/selling-module/src/sales-receipt/dto/create-sales-receipt.dto.ts
+++ microservices/selling-module/src/sales-receipt/dto/create-sales-receipt.dto.ts
@@ -1,1 +1,21 @@
-export class CreateSalesReceiptDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+class SalesReceiptLine {
+    idArticle:number
+    quantity:number
+    price:number
+}
+export class CreateSalesReceiptDto {
+    @ApiProperty()
+    deliveryDate:Date
+    @ApiProperty()
+    totalAmount:number
+    @ApiProperty()
+    idClient:number
+    @ApiProperty()
+    salesChannelId:number
+    @ApiProperty()
+    exitNoteId:number
+    @ApiProperty({type:[SalesReceiptLine]})
+    salesReceiptLine : SalesReceiptLine[]
+}
--- microservices/selling-module/src/sales-receipt/sales-receipt.service.ts
+++ microservices/selling-module/src/sales-receipt/sales-receipt.service.ts
@@ -1,26 +1,63 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSalesReceiptDto } from './dto/create-sales-receipt.dto';
 import { UpdateSalesReceiptDto } from './dto/update-sales-receipt.dto';
+import { PrismaService } from 'nestjs-prisma';
+import { ExitNote } from 'src/helpers/exitNote';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class SalesReceiptService {
-  create(createSalesReceiptDto: CreateSalesReceiptDto) {
-    return 'This action adds a new salesReceipt';
+  constructor(
+    private readonly prisma: PrismaService,
+    private helperExitNote: ExitNote,
+  ) {} 
+  async create(createSalesReceiptDto: CreateSalesReceiptDto) {
+    // return await this.prisma.$transaction(
+    //   async (prisma: Prisma.TransactionClient) => {
+    //     let { exitNoteId, salesReceiptLine, ...rest } =
+    //       createSalesReceiptDto;
+    //       console.log("exitNOteID", exitNoteId)
+    //     if (!exitNoteId) {
+    //       const newExitNote = await this.helperExitNote.create(
+    //         prisma,
+    //         {
+    //           saleChannelId: createSalesReceiptDto.salesChannelId,
+    //           date: createSalesReceiptDto.deliveryDate.toISOString(),
+    //           exitNoteLines: salesReceiptLine,
+    //           totalAmount:createSalesReceiptDto?.totalAmount
+    //         },
+    //       ); 
+    //       console.log('newExitNote', newExitNote)
+    //       exitNoteId = newExitNote.id;
+    //     }
+    //     return await prisma.salesReceipt.create({
+    //       data: {
+    //         ...rest,
+    //         salesReceiptLine: {
+    //           createMany: { data: salesReceiptLine },
+    //         },
+    //         exitNoteId
+    //       },
+    //     });
+    //   },
+    // );
   }
 
-  findAll() {
-    return `This action returns all salesReceipt`;
+ async findAll() {
+    let data = await this.prisma.salesReceipt.findMany();
+    let count = await this.prisma.salesReceipt.count()
+    return {data,count}
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} salesReceipt`;
+  async findOne(id: number) {
+    return await this.prisma.salesReceipt.findUnique({where:{id}});
   }
 
-  update(id: number, updateSalesReceiptDto: UpdateSalesReceiptDto) {
+  async update(id: number, updateSalesReceiptDto: UpdateSalesReceiptDto) {
     return `This action updates a #${id} salesReceipt`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} salesReceipt`;
   }
 }
